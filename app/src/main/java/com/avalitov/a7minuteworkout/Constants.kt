12d5a91893260bbb8a35db0ea3e37b10535fdaf9
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-07-09 15:38:56 +0500

    Exercise timer realized

---
+++ app/src/main/java/com/avalitov/a7minuteworkout/Constants.kt
@@ -0,0 +1,7 @@
+package com.avalitov.a7minuteworkout
+
+// global constants
+object Constants {
+    const val REST_TIME: Int = 4
+    const val EXERCISE_TIME: Int = 11
+}
--- app/src/main/java/com/avalitov/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/avalitov/a7minuteworkout/ExerciseActivity.kt
@@ -3,26 +3,32 @@ package com.avalitov.a7minuteworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.Toolbar
+import android.view.View
+import android.widget.*
 
+lateinit var llRestView : LinearLayout
+lateinit var llExerciseView : LinearLayout
 lateinit var toolbarExerciseActivity : androidx.appcompat.widget.Toolbar
-lateinit var tvTimer : TextView
+lateinit var tvTimerRest : TextView
+lateinit var tvTimerExercise : TextView
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var restTimer: CountDownTimer? = null
+    private var exerciseTimer: CountDownTimer? = null
     private var restProgress = 0
+    private var exerciseProgress = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
 
+        llRestView = findViewById(R.id.llRestView)
+        llExerciseView = findViewById(R.id.llExerciseView)
         toolbarExerciseActivity = findViewById(R.id.toolbar_exercise_activity)
-        tvTimer = findViewById(R.id.tv_timer)
+        tvTimerRest = findViewById(R.id.tv_timer_rest)
+        tvTimerExercise = findViewById(R.id.tv_timer_exercise)
 
         setSupportActionBar(toolbarExerciseActivity)
         val actionbar = supportActionBar
@@ -44,14 +50,14 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     private fun setRestProgressBar(){
-        var progressBar : ProgressBar = findViewById(R.id.progressBar)
-        progressBar.progress = restProgress
+        var progressBarRest : ProgressBar = findViewById(R.id.progressBar_rest)
+        progressBarRest.progress = restProgress
 
-        restTimer = object: CountDownTimer(10000, 1000){
+        restTimer = object: CountDownTimer((Constants.REST_TIME * 1000).toLong(), 1000){
             override fun onTick(millisUntilFinished: Long) {
                 restProgress++
-                progressBar.progress = 10 - restProgress
-                tvTimer.text = (10 - restProgress).toString()
+                progressBarRest.progress = Constants.REST_TIME - restProgress
+                tvTimerRest.text = (Constants.REST_TIME - restProgress).toString()
             }
             override fun onFinish() {
                 Toast.makeText(
@@ -59,6 +65,30 @@ class ExerciseActivity : AppCompatActivity() {
                     "Here now we will start the exercise.",
                     Toast.LENGTH_SHORT
                 ).show()
+
+                setupExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setExerciseProgressBar(){
+        var progressBarExercise : ProgressBar = findViewById(R.id.progressBar_exercise)
+        progressBarExercise.progress = exerciseProgress
+
+        exerciseTimer = object: CountDownTimer((Constants.EXERCISE_TIME * 1000).toLong(), 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                progressBarExercise.progress = Constants.EXERCISE_TIME - exerciseProgress
+                tvTimerExercise.text = (Constants.EXERCISE_TIME - exerciseProgress).toString()
+            }
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "The exercise has finished.",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                setupRestView()
             }
         }.start()
     }
@@ -69,7 +99,20 @@ class ExerciseActivity : AppCompatActivity() {
             restProgress = 0
         }
 
+        llExerciseView.visibility = View.INVISIBLE
+        llRestView.visibility = View.VISIBLE
         setRestProgressBar()
+    }
+
+    private fun setupExerciseView(){
+        if(exerciseTimer != null){
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+
+        llRestView.visibility = View.INVISIBLE
+        llExerciseView.visibility = View.VISIBLE
+        setExerciseProgressBar()
     }
 }
 
