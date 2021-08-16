91bb07f4e231f0fff8e83eaf71b45306919e780a
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-08-16 19:14:51 +0500

    Finish activity realised

--- app/src/main/java/com/avalitov/a7minuteworkout/FinishActivity.kt
+++ app/src/main/java/com/avalitov/a7minuteworkout/FinishActivity.kt
@@ -1,11 +1,26 @@
 package com.avalitov.a7minuteworkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.avalitov.a7minuteworkout.R
+import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
+
+        setSupportActionBar(toolbar_finish_activity)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        toolbar_finish_activity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        btnFinish.setOnClickListener {
+            finish()
+        }
     }
 }
