0f9ed22d72d25b4b915047361b423b1806c0175f
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-08-19 21:05:49 +0500

    History database realized

--- app/src/main/java/com/avalitov/a7minuteworkout/HistoryActivity.kt
+++ app/src/main/java/com/avalitov/a7minuteworkout/HistoryActivity.kt
@@ -2,6 +2,9 @@ package com.avalitov.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_history.*
 
 class HistoryActivity : AppCompatActivity() {
@@ -17,6 +20,27 @@ class HistoryActivity : AppCompatActivity() {
         }
         toolbar_history_activity.setNavigationOnClickListener {
             onBackPressed()
+        }
+
+        getAllCompletedDates()
+    }
+
+    private fun getAllCompletedDates() {
+        val dbHandler = SqliteOpenHelper(this, null)
+        val allCompletedDatesList = dbHandler.getAllCompleteDatesList()
+
+        if(allCompletedDatesList.size > 0) {
+            tv_history.visibility = View.VISIBLE
+            rv_history.visibility = View.VISIBLE
+            tv_no_data_available.visibility = View.GONE
+
+            rv_history.layoutManager = LinearLayoutManager(this)
+            val historyAdapter = HistoryAdapter(this, allCompletedDatesList)
+            rv_history.adapter = historyAdapter
+        } else {
+            tv_history.visibility = View.GONE
+            rv_history.visibility = View.GONE
+            tv_no_data_available.visibility = View.VISIBLE
         }
     }
 }
